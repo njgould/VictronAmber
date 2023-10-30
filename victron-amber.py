a0de9f306875c411cb49eee1dccbcbe40e3550d3
njgould <njgould@users.noreply.github.com>
2023-10-31 09:24:51 +1100

    imports

--- victron-amber.py
+++ victron-amber.py
@@ -20,7 +20,7 @@ import time
 import os
 import requests  # for http GET
 import pymodbus
-from pymodbus.client.sync import ModbusTcpClient as ModbusClient
+from pymodbus.client.sync import ModbusTcpClient
     
 
 try:
